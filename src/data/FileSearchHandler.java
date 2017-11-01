48cb051315731232acb87d14729bd7446d33573b
Sangyoon <rkfoql8516@gmail.com>
2017-11-01 16:14:01 +0900

    20171025 1830

--- src/data/FileSearchHandler.java
+++ src/data/FileSearchHandler.java
@@ -1,14 +1,17 @@
 package data;
 
+import com.sun.deploy.util.StringUtils;
 import helper.Helper;
 import org.json.simple.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 public class FileSearchHandler {
@@ -30,7 +33,6 @@ public class FileSearchHandler {
         try (Stream<String> lines = Files.lines(Paths.get(String.format("data/%d/data.txt", shard)))){
 
             for(String line : (Iterable<String>)lines::iterator){
-
                 JSONObject _j = Helper.encodeToJson(line);
                 if(_j.containsKey(key)){
                     if(_j.get(key).equals(value)){
@@ -47,5 +49,104 @@ public class FileSearchHandler {
         }
         return founded;
     }
+
+    public ArrayList<JSONObject> countSearch(JSONObject json, int shard){
+        ArrayList<JSONObject> founded = new ArrayList<>();
+        String key = json.get("key").toString();
+        double time1 = Double.parseDouble(json.get("time1").toString());
+        double time2 = Double.parseDouble(json.get("time2").toString());
+        try (Stream<String> lines = Files.lines(Paths.get(String.format("data/%d/data.txt", shard)))){
+
+            for(String line : (Iterable<String>)lines::iterator){
+                JSONObject _j = Helper.encodeToJson(line);
+                JSONObject temp = new JSONObject();
+                double time = Double.parseDouble(_j.get("@timestamp").toString());
+                if(_j.containsKey(key) && time1 <= time && time <= time2){
+                    temp.put(_j.get(key).toString(), 1);
+                    founded.add(temp);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            return founded;
+        } catch(NoSuchFileException e){
+            return founded;
+        } catch (IOException e) {
+            return founded;
+        }
+        return founded;
+    }
+
+    public ArrayList<JSONObject> maxSearch(JSONObject json, int shard){
+        ArrayList<JSONObject> founded = new ArrayList<>();
+        String key = json.get("key").toString();
+        double time1 = Double.parseDouble(json.get("time1").toString());
+        double time2 = Double.parseDouble(json.get("time2").toString());
+
+        try (Stream<String> lines = Files.lines(Paths.get(String.format("data/%d/data.txt", shard)))){
+
+
+            Iterator<String> iter = lines.iterator();
+            if (!iter.hasNext()){
+                return founded;
+            }
+
+            JSONObject maxValue = Helper.encodeToJson(iter.next());
+
+            for(String line : (Iterable<String>)lines::iterator){
+                JSONObject _j = Helper.encodeToJson(line);
+                double time = Double.parseDouble(_j.get("@timestamp").toString());
+                if(_j.containsKey(key) && time1 <= time && time <= time2) {
+                    if ((Long) _j.get(key) > (Long) maxValue.get(key)) {
+                        maxValue = _j;
+                    }
+                }
+            }
+            founded.add(maxValue);
+        } catch (FileNotFoundException e) {
+            return founded;
+        } catch(NoSuchFileException e){
+            return founded;
+        } catch (IOException e) {
+            return founded;
+        }
+        return founded;
+    }
+
+    public ArrayList<JSONObject> minSearch(JSONObject json, int shard){
+        ArrayList<JSONObject> founded = new ArrayList<>();
+        String key = json.get("key").toString();
+        double time1 = Double.parseDouble(json.get("time1").toString());
+        double time2 = Double.parseDouble(json.get("time2").toString());
+
+        try (Stream<String> lines = Files.lines(Paths.get(String.format("data/%d/data.txt", shard)))){
+
+            Iterator<String> iter = lines.iterator();
+            if (!iter.hasNext()){
+                return founded;
+            }
+
+            JSONObject minValue = Helper.encodeToJson(iter.next());
+
+            for(String line : (Iterable<String>)lines::iterator){
+                JSONObject _j = Helper.encodeToJson(line);
+                double time = Double.parseDouble(_j.get("@timestamp").toString());
+                if(_j.containsKey(key) && time1 <= time && time <= time2){
+                    if ((Long) _j.get(key) < (Long) minValue.get(key)) {
+                        minValue = _j;
+                    }
+                }
+            }
+            founded.add(minValue);
+
+        } catch (FileNotFoundException e) {
+            return founded;
+        } catch(NoSuchFileException e){
+            return founded;
+        } catch (IOException e) {
+            return founded;
+        }
+        return founded;
+    }
+
 }
 
